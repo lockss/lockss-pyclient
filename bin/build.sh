9d089da3ae09237cf8a6404f5acf58f55e8db0b7
Thib Guicherd-Callin <thib@cs.stanford.edu>
2025-10-31 14:57:40 -0700

    Include the Swagger spec as a resource.

--- bin/build.sh
+++ bin/build.sh
@@ -18,10 +18,15 @@ fi
 for lockss_svc in "${LOCKSS_REST_SERVICES[@]}"; do
   PROJECT_DIR="${LAAWS_BUILD_DIR}/${lockss_svc}"
   PYTHON_CLIENT_SRC="${PROJECT_DIR}/target/swagger_codegen_python"
+  PROJECT_CODE="$(basename "${PYTHON_CLIENT_SRC}/lockss/pyclient/"*)"
 
   # Invoke Swagger codegen plugin in Maven to generate Python clients
   ( cd  ${PROJECT_DIR} && mvn generate-sources -DskipSwagger=true -DskipSwaggerPython=false )
 
   # Aggregate Python client sources into this src/ tree
   rsync -a -v ${PYTHON_CLIENT_SRC}/lockss/ src/lockss/
+
+  # Add Swagger spec as resource
+  SWAGGER="${PROJECT_DIR}/src/main/resources/swagger/swagger.yaml"
+  cp -v "${SWAGGER}" "src/lockss/pyclient/${PROJECT_CODE}/"
 done
