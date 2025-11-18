2e7a5131b5c00c3e5cbaa12252ca454ac99b4182
Thib Guicherd-Callin <thib@cs.stanford.edu>
2025-11-18 11:11:41 -0800

    Tilde expansion: use `pwd` to generate project code.

--- bin/build.sh
+++ bin/build.sh
@@ -18,7 +18,7 @@ fi
 for lockss_svc in "${LOCKSS_REST_SERVICES[@]}"; do
   PROJECT_DIR="${LAAWS_BUILD_DIR}/${lockss_svc}"
   PYTHON_CLIENT_SRC="${PROJECT_DIR}/target/swagger_codegen_python"
-  PROJECT_CODE="$(basename "${PYTHON_CLIENT_SRC}/lockss/pyclient/"*)"
+  PROJECT_CODE="$(cd "${PYTHON_CLIENT_SRC}/lockss/pyclient/"* && basename $(pwd))"
 
   # Invoke Swagger codegen plugin in Maven to generate Python clients
   ( cd  ${PROJECT_DIR} && mvn generate-sources -DskipSwagger=true -DskipSwaggerPython=false )
