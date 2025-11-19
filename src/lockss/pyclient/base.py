847e0474629b90f621d53d4225e5b39ec4b2bcbb
Thib Guicherd-Callin <thib@cs.stanford.edu>
2025-11-19 10:36:28 -0800

    Add secondary params. Propagate paging-related params. Add getStorageInfo. Cosmetic: redefine _first, shorten Swagger variables, spacing.

--- src/lockss/pyclient/base.py
+++ src/lockss/pyclient/base.py
@@ -56,26 +56,26 @@ def _load_swagger(package) -> YamlT:
             return yaml.safe_load(f)
 
 
-__CONFIG_SWAGGER = _load_swagger(config)
+__CONFIG = _load_swagger(config)
 
 
-__CRAWLER_SWAGGER = _load_swagger(crawler)
+__CRAWLER = _load_swagger(crawler)
 
 
-__MD_SWAGGER = _load_swagger(md)
+__MD = _load_swagger(md)
 
 
-__POLLER_SWAGGER = _load_swagger(poller)
+__POLLER = _load_swagger(poller)
 
 
-__RS_SWAGGER = _load_swagger(rs)
+__RS = _load_swagger(rs)
 
 
-def _first(json_path: str, data: YamlT) -> Any:
+def _first(data: YamlT, json_path: str) -> Any:
     return query(json_path, data).first_one().value
 
 
-RS_DEFAULT_PORT: int = _first('$.servers[0].variables.port.default', __RS_SWAGGER)
+RS_DEFAULT_PORT: int = _first(__RS, '$.servers[0].variables.port.default')
 
 
 ConfT = Union[
@@ -105,7 +105,8 @@ class Node(object):
     def _make_conf_template(make_conf: ConfSupplier,
                             get_port: PortGetter):
         def decorate(f):
-            def decorated_make_conf(self: Node, needs_auth: bool = True) -> ConfT:
+            def decorated_make_conf(self: Node,
+                                    needs_auth: bool = True) -> ConfT:
                 conf = make_conf()
                 conf.host = f'{self.get_host()}:{get_port(self)}'
                 if needs_auth:
@@ -119,7 +120,8 @@ class Node(object):
     @staticmethod
     def _set_port_template(port_name: str):
         def decorate(f):
-            def decorated_set_port(self: Node, port: int) -> Node:
+            def decorated_set_port(self: Node,
+                                   port: int) -> Node:
                 setattr(self, port_name, port)
                 return self
             return decorated_set_port
@@ -267,7 +269,7 @@ def _paged_request_iterator_template(single_request: PageInfoResultFunction,
                     kwargs['continuation_token'] = token
                 else:
                     kwargs.pop('continuation_token', None)
-                if 'limit' not in kwargs:
+                if kwargs.get('limit') is None:
                     kwargs['limit'] = 100
                 result: PageInfoResultT = single_request(*args, **kwargs)
                 token = result.page_info.continuation_token
@@ -280,11 +282,21 @@ def _paged_request_iterator_template(single_request: PageInfoResultFunction,
 
 def repo_get_artifact_by_uuid(node: Node,
                               uuid: str,
-                              namespace: str = _first('$.paths["/artifacts/{uuid}"].get.parameters[?(@.name == "namespace")].schema.default', __RS_SWAGGER)) -> MultipartParser:
-    @_single_request_template(Node.make_repo_conf, rs.ApiClient, rs.ArtifactsApi, rs.ArtifactsApi.get_artifact_data_by_multipart)
-    def _repo_get_artifact_by_uuid(node: Node, uuid: str, namespace: str = None) -> str:
+                              namespace: str = _first(__RS, '$.paths["/artifacts/{uuid}"].get.parameters[?(@.name == "namespace")].schema.default'),
+                              include_content: rs.IncludeContentEnum = _first(__RS, '$.components.schemas.includeContentEnum.default')) -> MultipartParser:
+    @_single_request_template(Node.make_repo_conf,
+                              rs.ApiClient,
+                              rs.ArtifactsApi,
+                              rs.ArtifactsApi.get_artifact_data_by_multipart)
+    def _repo_get_artifact_by_uuid(node: Node,
+                                   uuid: str,
+                                   namespace: str = None,
+                                   include_content: rs.IncludeContentEnum = None) -> str:
         pass
-    result: str = _repo_get_artifact_by_uuid(node, uuid, namespace=namespace)
+    result: str = _repo_get_artifact_by_uuid(node,
+                                             uuid,
+                                             namespace=namespace,
+                                             include_content=include_content)
     # Result is of type str but seems to be a repr() string "b'...'"
     boundary = (byte_input := eval(result)).partition(b'\r\n')[0].partition(b'--')[2]
     return MultipartParser(BytesIO(byte_input), boundary)
@@ -292,54 +304,106 @@ def repo_get_artifact_by_uuid(node: Node,
 
 def repo_get_artifact_response_by_uuid(node: Node,
                                        uuid: str,
-                                       namespace: str = _first('$.paths["/artifacts/{uuid}/response"].get.parameters[?(@.name == "namespace")].schema.default', __RS_SWAGGER)) -> str:
-    @_single_request_template(Node.make_repo_conf, rs.ApiClient, rs.ArtifactsApi, rs.ArtifactsApi.get_artifact_data_by_response)
-    def _repo_get_artifact_response_by_uuid(node: Node, uuid: str, namespace: str = None) -> str:
+                                       namespace: str = _first(__RS, '$.paths["/artifacts/{uuid}/response"].get.parameters[?(@.name == "namespace")].schema.default'),
+                                       include_content: rs.IncludeContentEnum = _first(__RS, '$.components.schemas.includeContentEnum.default')) -> str:
+    @_single_request_template(Node.make_repo_conf,
+                              rs.ApiClient,
+                              rs.ArtifactsApi,
+                              rs.ArtifactsApi.get_artifact_data_by_response)
+    def _repo_get_artifact_response_by_uuid(node: Node,
+                                            uuid: str,
+                                            namespace: str = None,
+                                            include_content: rs.IncludeContentEnum = None) -> str:
         pass
-    result: str = _repo_get_artifact_response_by_uuid(node, uuid, namespace=namespace, include_content='NEVER')
+    result: str = _repo_get_artifact_response_by_uuid(node,
+                                                      uuid,
+                                                      namespace=namespace,
+                                                      include_content=include_content)
     # Result is of type str but seems to be a repr() string "b'...'"
     return eval(result).decode()
 
 
 def repo_get_artifact_payload_by_uuid(node: Node,
-                                       uuid: str,
-                                       namespace: str = _first('$.paths["/artifacts/{uuid}/payload"].get.parameters[?(@.name == "namespace")].schema.default', __RS_SWAGGER)) -> bytes:
-    @_single_request_template(Node.make_repo_conf, rs.ApiClient, rs.ArtifactsApi, rs.ArtifactsApi.get_artifact_data_by_payload)
-    def _repo_get_artifact_payload_by_uuid(node: Node, uuid: str, namespace: str = None) -> str:
+                                      uuid: str,
+                                      namespace: str = _first(__RS, '$.paths["/artifacts/{uuid}/payload"].get.parameters[?(@.name == "namespace")].schema.default'),
+                                      include_content: rs.IncludeContentEnum = _first(__RS, '$.components.schemas.includeContentEnum.default')) -> bytes:
+    @_single_request_template(Node.make_repo_conf,
+                              rs.ApiClient,
+                              rs.ArtifactsApi,
+                              rs.ArtifactsApi.get_artifact_data_by_payload)
+    def _repo_get_artifact_payload_by_uuid(node: Node,
+                                           uuid: str,
+                                           namespace: str = None) -> str:
         pass
-    result: str = _repo_get_artifact_payload_by_uuid(node, uuid, namespace=namespace, include_content='ALWAYS')
+    result: str = _repo_get_artifact_payload_by_uuid(node,
+                                                     uuid, namespace=namespace,
+                                                     include_content=include_content)
     # Result is of type str but seems to be a repr() string "b'...'"
     return eval(result)
 
 
-@_single_request_template(Node.make_repo_conf, rs.ApiClient, rs.ArtifactsApi, rs.ArtifactsApi.get_artifacts)
+@_single_request_template(Node.make_repo_conf,
+                          rs.ApiClient,
+                          rs.ArtifactsApi,
+                          rs.ArtifactsApi.get_artifacts,
+                          remove_kwargs=['url', 'url_prefix'])
 def repo_get_artifacts_by_auid_page(node: Node,
                                     auid: str,
-                                    namespace: str = _first('$.paths["/aus/{auid}/artifacts"].get.parameters[?(@.name == "namespace")].schema.default', __RS_SWAGGER)) -> rs.ArtifactPageInfo:
+                                    url: Optional[str] = None,
+                                    url_prefix: Optional[str] = None,
+                                    namespace: str = _first(__RS, '$.paths["/aus/{auid}/artifacts"].get.parameters[?(@.name == "namespace")].schema.default'),
+                                    version: Optional[Union[int, rs.VersionsEnum]] = None,
+                                    include_uncommitted: Optional[bool] = None,
+                                    limit: Optional[int] = None,
+                                    continuation_token: Optional[str] = None) -> rs.ArtifactPageInfo:
     pass
 
 
 @_paged_request_iterator_template(repo_get_artifacts_by_auid_page)
 def repo_get_artifacts_by_auid_page_iter(node: Node,
                                          auid: str,
-                                         namespace: str = _first('$.paths["/aus/{auid}/artifacts"].get.parameters[?(@.name == "namespace")].schema.default', __RS_SWAGGER)) -> Iterable[rs.ArtifactPageInfo]:
+                                         url: Optional[str] = None,
+                                         url_prefix: Optional[str] = None,
+                                         namespace: str = _first(__RS, '$.paths["/aus/{auid}/artifacts"].get.parameters[?(@.name == "namespace")].schema.default'),
+                                         version: Optional[Union[int, rs.VersionsEnum]] = None,
+                                         include_uncommitted: Optional[bool] = None,
+                                         limit: Optional[int] = None) -> Iterable[rs.ArtifactPageInfo]:
     pass
 
 
 def repo_get_artifacts_by_auid(node: Node,
                                auid: str,
-                               namespace: str = _first('$.paths["/aus"].get.parameters[?(@.name == "namespace")].schema.default', __RS_SWAGGER)) -> list[rs.Artifact]:
+                               url: Optional[str] = None,
+                               url_prefix: Optional[str] = None,
+                               namespace: str = _first(__RS, '$.paths["/aus"].get.parameters[?(@.name == "namespace")].schema.default'),
+                               version: Optional[Union[int, rs.VersionsEnum]] = None,
+                               include_uncommitted: Optional[bool] = None,
+                               limit: Optional[int] = None) -> list[rs.Artifact]:
     ret = []
-    for page in repo_get_artifacts_by_auid_page_iter(node, auid, namespace=namespace):
+    for page in repo_get_artifacts_by_auid_page_iter(node,
+                                                     auid,
+                                                     url=url,
+                                                     url_prefix=url_prefix,
+                                                     namespace=namespace,
+                                                     version=version,
+                                                     include_uncommitted=include_uncommitted,
+                                                     limit=limit):
         ret.extend(page.artifacts)
     return ret
 
 
-@_single_request_template(Node.make_repo_conf, rs.ApiClient, rs.ArtifactsApi, rs.ArtifactsApi.get_artifacts_from_all_aus, remove_kwargs=['url', 'url_prefix'])
+@_single_request_template(Node.make_repo_conf,
+                          rs.ApiClient,
+                          rs.ArtifactsApi,
+                          rs.ArtifactsApi.get_artifacts_from_all_aus,
+                          remove_kwargs=['url', 'url_prefix'])
 def repo_get_artifacts_by_url_page(node: Node,
                                    url: Optional[str] = None,
                                    url_prefix: Optional[str] = None,
-                                   namespace: Optional[str] = _first('$.paths["/artifacts"].get.parameters[?(@.name == "namespace")].schema.default', __RS_SWAGGER)) -> rs.ArtifactPageInfo:
+                                   namespace: str = _first(__RS, '$.paths["/artifacts"].get.parameters[?(@.name == "namespace")].schema.default'),
+                                   versions: rs.VersionsEnum = _first(__RS, '$.components.schemas.versionsEnum.default'),
+                                   limit: Optional[int] = None,
+                                   continuation_token: Optional[str] = None) -> rs.ArtifactPageInfo:
     pass
 
 
@@ -347,65 +411,106 @@ def repo_get_artifacts_by_url_page(node: Node,
 def repo_get_artifacts_by_url_page_iter(node: Node,
                                         url: Optional[str] = None,
                                         url_prefix: Optional[str] = None,
-                                        namespace: Optional[str] = _first('$.paths["/artifacts"].get.parameters[?(@.name == "namespace")].schema.default', __RS_SWAGGER)) -> Iterable[rs.ArtifactPageInfo]:
+                                        namespace: str = _first(__RS, '$.paths["/artifacts"].get.parameters[?(@.name == "namespace")].schema.default'),
+                                        versions: rs.VersionsEnum = _first(__RS, '$.components.schemas.versionsEnum.default'),
+                                        limit: Optional[int] = None) -> Iterable[rs.ArtifactPageInfo]:
     pass
 
 
 def repo_get_artifacts_by_url(node: Node,
                               url: Optional[str] = None,
                               url_prefix: Optional[str] = None,
-                              namespace: Optional[str] = _first('$.paths["/artifacts"].get.parameters[?(@.name == "namespace")].schema.default', __RS_SWAGGER)) -> list[rs.Artifact]:
+                              namespace: str = _first(__RS, '$.paths["/artifacts"].get.parameters[?(@.name == "namespace")].schema.default'),
+                              versions: rs.VersionsEnum = _first(__RS, '$.components.schemas.versionsEnum.default'),
+                              limit: Optional[int] = None) -> list[rs.Artifact]:
     ret = []
-    for page in repo_get_artifacts_by_url_page_iter(node, url=url, url_prefix=url_prefix, namespace=namespace):
+    for page in repo_get_artifacts_by_url_page_iter(node,
+                                                    url=url,
+                                                    url_prefix=url_prefix,
+                                                    namespace=namespace,
+                                                    versions=versions,
+                                                    limit=limit):
         ret.extend(page.artifacts)
     return ret
 
 
-@_single_request_template(Node.make_repo_conf, rs.ApiClient, rs.AusApi, rs.AusApi.get_artifacts_size)
+@_single_request_template(Node.make_repo_conf,
+                          rs.ApiClient,
+                          rs.AusApi,
+                          rs.AusApi.get_artifacts_size)
 def repo_get_au_size(node: Node,
                      auid: str,
-                     namespace: Optional[str] = _first('$.paths["/aus/{auid}/size"].get.parameters[?(@.name == "namespace")].schema.default', __RS_SWAGGER)) -> rs.AuSize:
+                     namespace: str = _first(__RS, '$.paths["/aus/{auid}/size"].get.parameters[?(@.name == "namespace")].schema.default')) -> rs.AuSize:
     pass
 
 
-@_single_request_template(Node.make_repo_conf, rs.ApiClient, rs.AusApi, rs.AusApi.get_aus)
+@_single_request_template(Node.make_repo_conf,
+                          rs.ApiClient,
+                          rs.AusApi,
+                          rs.AusApi.get_aus)
 def repo_get_auids_page(node: Node,
-                        namespace: Optional[str] = _first('$.paths["/aus"].get.parameters[?(@.name == "namespace")].schema.default', __RS_SWAGGER),
-                        **kwargs) -> rs.AuidPageInfo:
+                        namespace: str = _first(__RS, '$.paths["/aus"].get.parameters[?(@.name == "namespace")].schema.default'),
+                        limit: Optional[int] = None,
+                        continuation_token: Optional[str] = None) -> rs.AuidPageInfo:
     pass
 
 
 @_paged_request_iterator_template(repo_get_auids_page)
 def repo_get_auids_page_iter(node: Node,
-                             namespace: Optional[str] = _first('$.paths["/aus"].get.parameters[?(@.name == "namespace")].schema.default', __RS_SWAGGER)) -> Iterable[rs.AuidPageInfo]:
+                             namespace: str = _first(__RS, '$.paths["/aus"].get.parameters[?(@.name == "namespace")].schema.default'),
+                             limit: Optional[int] = None) -> Iterable[rs.AuidPageInfo]:
     pass
 
 
 def repo_get_auids(node: Node,
-                   namespace: Optional[str] = _first('$.paths["/aus"].get.parameters[?(@.name == "namespace")].schema.default', __RS_SWAGGER)) -> list[str]:
+                   namespace: str = _first(__RS, '$.paths["/aus"].get.parameters[?(@.name == "namespace")].schema.default'),
+                   limit: Optional[int] = None) -> list[str]:
     ret = []
-    for page in repo_get_auids_page_iter(node, namespace=namespace):
+    for page in repo_get_auids_page_iter(node,
+                                         namespace=namespace,
+                                         limit=limit):
         ret.extend(page.auids)
     return ret
 
 
-@_single_request_template(Node.make_repo_conf, rs.ApiClient, rs.RepoApi, rs.RepoApi.get_supported_checksum_algorithms)
+@_single_request_template(Node.make_repo_conf,
+                          rs.ApiClient,
+                          rs.RepoApi,
+                          rs.RepoApi.get_supported_checksum_algorithms)
 def repo_get_checksum_algorithms(node: Node) -> list[str]:
     pass
 
 
-@_single_request_template(Node.make_repo_conf, rs.ApiClient, rs.RepoApi, rs.RepoApi.get_repository_information)
+@_single_request_template(Node.make_repo_conf,
+                          rs.ApiClient,
+                          rs.RepoApi,
+                          rs.RepoApi.get_repository_information)
 def repo_get_info(node: Node) -> rs.RepositoryInfo:
     pass
 
 
-@_single_request_template(Node.make_repo_conf, rs.ApiClient, rs.RepoApi, rs.RepoApi.get_namespaces)
+@_single_request_template(Node.make_repo_conf,
+                          rs.ApiClient,
+                          rs.RepoApi,
+                          rs.RepoApi.get_namespaces)
 def repo_get_namespaces(node: Node) -> list[str]:
     pass
 
 
-@_single_request_template(Node.make_repo_conf, rs.ApiClient, rs.StatusApi, rs.StatusApi.get_status, needs_auth=False)
+@_single_request_template(Node.make_repo_conf,
+                          rs.ApiClient,
+                          rs.StatusApi,
+                          rs.StatusApi.get_status,
+                          needs_auth=False)
 def repo_get_status(node: Node) -> rs.ApiStatus:
+    pass
+
+
+@_single_request_template(Node.make_repo_conf,
+                          rs.ApiClient,
+                          rs.RepoApi,
+                          rs.RepoApi.get_storage_info)
+def repo_get_storage_info(node: Node) -> rs.StorageInfo:
     pass
 
 
